5c1d477bebfc5761dfe934159b99a5e49644e294
Ingrid Gomo <54813201+IngridGomo@users.noreply.github.com>
2020-12-09 20:26:06 +0100

    started navbar css

--- gsm_navbar.js
+++ gsm_navbar.js
@@ -1,68 +1,51 @@
-//Javascript for header/navbar
-
-// target for å opprette navbar i html.
-const headertarget = document.querySelector("#js-header");
-createNavbar(headertarget);
-
+const headerTarget = document.querySelector("#header");
+createNavbar(headerTarget);
 
 function createNavbar(targetElement) {
-    createModElement("nav", targetElement, "", "", ["id", "js-navbar"]);
+    createModElement("nav", targetElement, "", "", ["id", "navbar"]);
+    const navbar = document.querySelector("#navbar"); //refererer til den over
 
-    const navbar = document.querySelector("#js-navbar");
-    createModElement("a", navbar, "", "index.html", ["id", "js-header-logo"]);
+    createModElement("a", navbar, "", "gsm_forside.html", ["id", "header_logo"]);
+    const logo = document.querySelector("#header_logo");
 
-    const logo = document.querySelector("#js-header-logo");
-    createModElement("img", logo, "", "", [["src", "img/pareto_logo.png"], ["id", "js-header-logoimg"], ["alt", "Pareto-logo"]]);
-    createModElement("ul", navbar, "", "", ["id", "js-header-ul"]);
+    createModElement("img", logo, "", "", [["src", "./img/gsm_logo.jpg"], ["id", "header_logoImg"], ["alt", "goSupermodel_logo"]]);
 
-    for (i = 0; i < 5; i++) {
-        createModElement("li", document.querySelector("#js-header-ul"), "", "", ["class", "js-header-li"]);
+    createModElement("ul", navbar, "", "", ["id", "header_tabUl"]);
+    for (i = 0; i < 4; i++) {
+        createModElement("li", document.querySelector("#header_tabUl"), "", "", ["class", "header_tabLi"]);
     }
+    const tabLiElements = document.getElementsByClassName("header_tabLi");
 
-    const lielements = document.getElementsByClassName("js-header-li");
-    /* createModElement("a", lielements[0], "Om Pareto", "om_pareto.html", [["class", "js-header-link"], "id", "js-dropdown"]); */
-    createModElement("div",lielements[0],"","",[["class","dropdown"],["id","js-dropcontainer"]]);
-    createModElement("a",document.getElementById("js-dropcontainer"),"Om oss ▼","",[["onclick","dropdownFunction()"],["class","dropdownButton js-header-link"],["id","dropdownOmOss"]])
-    createModElement("div",document.getElementById("js-dropcontainer"),"","",[["id","Dropdown"],["class","dropdownItems"]])
-    createModElement("a",document.getElementById("Dropdown"),"Om Pareto","om_pareto.html",["class","js-header-link"])
-    createModElement("a",document.getElementById("Dropdown"),"Komiteer","komiteer.html",["class","js-header-link"])
-    createModElement("a",document.getElementById("Dropdown"),"Styret","styret.html",["class","js-header-link"])
-    createModElement("a",document.getElementById("Dropdown"),"Vedtekter","vedtekter.html",["class","js-header-link"])
+    createModElement("div", navbar, "", "", ["id", "header_downbar"]);
 
-    // virker som at dette er linkene til fanene liksom
-    // Nå setter jeg det som hovedfanene på gsm
-    createModElement("a", lielements[1], "Forside", "gsm_forside.html", ["class", "js-header-link"]);
-    createModElement("a", lielements[2], "Shopping", "gsm_shopping.html", ["class", "js-header-link"]);
-    createModElement("a", lielements[3], "Spill", "gsm_spill.html", ["class", "js-header-link"]);
-    createModElement("a", lielements[4], "Forum", "gsm_forum.html", ["class", "js-header-link"]);
-
-    // Oppretter knapp og img-element for rollup.js
-    createModElement("button",navbar,"","",[["id","js-scrollup"],["title","Gå til toppen"]]); /* !Button for scrollup! */
-    createModElement("img",document.getElementById("js-scrollup"),"","",[["src","img/scrollupikon.jpg"],["alt","scrollicon"],["width","50"],["height","50"]]);
-
-    // Oppretter knapp som viser/gjemmer navbar for mobil. 
-    createModElement("a",navbar,"","#",["id","js-menyknapp"]);
-    createModElement("img",document.getElementById("js-menyknapp"),"","",[["id","js-menysvg"],["src","img/ico/menyknapp.svg"],["alt","Menyknapp"],["width","48"],["height","48"],["type","image/svg+xml"]])
-}
+    // Definerer tabsene
+    createModElement("a", tabLiElements[0], "Forside", "gsm_forside.html", ["class", "header_tab"]);
+    createModElement("a", tabLiElements[1], "Shopping", "gsm_shopping.html", ["class", "header_tab"]);
+    createModElement("a", tabLiElements[2], "Spill", "gsm_spill.html", ["class", "header_tab"]);
+    createModElement("a", tabLiElements[3], "Forum", "gsm_forum.html", ["class", "header_tab"]);
 
 
-// eleattr = enten et array med ["attribute","verdi"] eller matrise [["attribute","verdi"],["attribute","verdi"], ..., ...]
-function createModElement(tagName, target, content = "", url = "", eleattr = "") {
-    const node = document.createElement(tagName);
-    const textnode = document.createTextNode(content);
-    node.appendChild(textnode);
+    // Lager en ting (f.eks. "a", "div") med det den skulle trenge av spesifikasjoner
+    // attr = enten et array med ["attribute","verdi"] eller matrise [["attribute","verdi"],
+        // ["attribute","verdi"], ..., ...]
+    function createModElement(htmlElement, target, content = "", url = "", attr = "") {
+    const node = document.createElement(htmlElement); // f.eks. "a", "div"
+    const textnode = document.createTextNode(content); // definerer teksten
+    node.appendChild(textnode); //legger til teksten i tingen (f.eks. i en "a")
     if (url !== "") {
-        node.setAttribute("href", url);
+        node.setAttribute("href", url); //setter link til den oppgitte urlen
     }
-    if (Array.isArray(eleattr)) {
-        if (Array.isArray(eleattr[0])) {
-            for (i = 0; i < eleattr.length; i++) {
-                node.setAttribute(eleattr[i][0], eleattr[i][1]);
+    if (Array.isArray(attr)) { //tror denne sjekker om den har noe innhold
+        if (Array.isArray(attr[0])) { //tror denne sjekker om det er en matise
+            for (i = 0; i < attr.length; i++) {
+                node.setAttribute(attr[i][0], attr[i][1]); //setter flere attributter til tingen
             }
         }
         else {
-            node.setAttribute(eleattr[0], eleattr[1]);
+            node.setAttribute(attr[0], attr[1]); //er ikke en matrise, setter attributt tin tingen
         }
     }
+ 
     target.appendChild(node);
+    }
 }
